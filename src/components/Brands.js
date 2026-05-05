d9587b9f1751df60cdc0e63d36f9d91df37e3f56
Rafasanna <rafaelasanna1511@gmail.com>
2026-05-05 18:01:53 -0300

    Agregados logos SVG oficiales de marcas

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -4,22 +4,20 @@ export default function Brands() {
   return (
     <div className={styles.brandsContainer}>
       <div className={`${styles.brand} ${styles.stella}`}>
-        Stella Artois
+        <img src="/images/stella.svg" alt="Stella Artois logo" className={styles.brandImage} />
       </div>
       
       <div className={`${styles.brand} ${styles.corona}`}>
-        <img src="/images/corona_logo.png" alt="Corona logo" className={styles.brandImage} />
-        Corona
+        <img src="/images/corona.svg" alt="Corona logo" className={styles.brandImage} />
       </div>
 
       <div className={`${styles.brand} ${styles.patagonia}`}>
         <img src="/images/patagonia_logo.png" alt="Patagonia logo" className={styles.brandImage} />
-        Patagonia
+        <span>Patagonia</span>
       </div>
 
       <div className={`${styles.brand} ${styles.pepsi}`}>
-        <div className={styles.pepsiCircle}></div>
-        pepsi
+        <img src="/images/pepsi.svg" alt="Pepsi logo" className={styles.brandImage} />
       </div>
     </div>
   );
